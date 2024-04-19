1f440114b69b6d4501cfc5a3ba69cfb9a5c9c85d
CasperHuikeshoven <casperhuik05@gmail.com>
2024-04-19 12:41:32 +0200

    Created a Colourmap and a way to create islands

--- Assets/scripts/MapGenerator.cs
+++ Assets/scripts/MapGenerator.cs
@@ -4,18 +4,79 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    
+
+    public enum DrawMode {NoiseMap, ColourMap, FalloffMap};
+    public DrawMode drawMode;
+    public bool island;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
-    public void GenerateMap(){
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+    public TerrainType[] regions;
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+    public void Start(){
+        GenerateMap();
     }
 
+    public void GenerateMap(){
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset, island);
+        float[,] falloffMap = FalloffGenerator.GenerateFallOffMap(mapWidth);
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for(int y = 0; y < mapHeight; y++){
+            for(int x = 0; x < mapWidth; x++){
+                float currentHeight = noiseMap[x,y];
+                if(island) currentHeight -= falloffMap[x,y];
+                for(int i = 0; i < regions.Length; i++){
+                    if(currentHeight <= regions[i].height){
+                        colourMap[y * mapWidth + x] = regions [i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if(drawMode == DrawMode.NoiseMap){
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if(drawMode == DrawMode.ColourMap){
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        } else if(drawMode == DrawMode.FalloffMap){
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFallOffMap(mapWidth)));
+        }
+        
+    }
+
+    void OnValidate(){
+        if(mapWidth < 1){
+            mapWidth = 1;
+        }
+        if(mapHeight < 1){
+            mapHeight = 1;
+        }
+        if(lacunarity < 1){
+            lacunarity = 1;
+        }
+        if(octaves < 0){
+            octaves = 0;
+        }
+    }
+
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color colour;
 }
