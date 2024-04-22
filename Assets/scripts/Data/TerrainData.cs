02e8b70240123d2967d7f75bb5352617fbe10dea
CasperHuikeshoven <casperhuik05@gmail.com>
2024-04-22 22:33:37 +0200

    Added textures to the terrain

--- Assets/scripts/Data/TerrainData.cs
+++ Assets/scripts/Data/TerrainData.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu()]
 public class TerrainData : ScriptableObject
 {
+
+    const int textureSize = 512;
+    const TextureFormat textureFormat = TextureFormat.RGB565;
 
     [Header("Terrain")]
     public bool island;
@@ -26,11 +30,7 @@ public class TerrainData : ScriptableObject
     public Vector2 offset;
 
     [Header("Shader")]
-    public Color[] baseColours;
-    [Range(0,1)]
-    public float[] baseStartHeights;
-    [Range(0,1)]
-    public float[] baseBlends;
+    public Layer[] layers;
 
     public float minHeight
     {
@@ -50,10 +50,14 @@ public class TerrainData : ScriptableObject
 
     public void ApplyToMaterial(Material material){
 
-        material.SetInt("baseColourCount", baseColours.Length);
-        material.SetColorArray("baseColours", baseColours);
-        material.SetFloatArray("baseStartHeights", baseStartHeights);
-        material.SetFloatArray("baseBlends", baseBlends);
+        material.SetInt("layerCount", layers.Length);
+        material.SetColorArray("baseColours", layers.Select(x=>x.tint).ToArray());
+        material.SetFloatArray("baseStartHeights", layers.Select(x=>x.startHeight).ToArray());
+        material.SetFloatArray("baseBlends", layers.Select(x=>x.blendStrength).ToArray());
+        material.SetFloatArray("baseColourStrength", layers.Select(x=>x.tintStrength).ToArray());
+        material.SetFloatArray("baseTextureScales", layers.Select(x=>x.textureScale).ToArray());
+        Texture2DArray texturesArray = GenerateTextureArray(layers.Select(x=>x.texture).ToArray());
+        material.SetTexture("baseTextures", texturesArray);
 
         UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
     }
@@ -64,6 +68,15 @@ public class TerrainData : ScriptableObject
 
         material.SetFloat("minHeight", minHeight);
         material.SetFloat("maxHeight", maxHeight);
+    }
+
+    Texture2DArray GenerateTextureArray(Texture2D[] textures){
+        Texture2DArray textureArray = new Texture2DArray(textureSize, textureSize, textures.Length, textureFormat, true);
+        for(int i = 0; i < textures.Length; i++){
+            textureArray.SetPixels(textures[i].GetPixels(), i);
+        }
+        textureArray.Apply();
+        return textureArray;
     }
 
     void OnValidate()
@@ -79,4 +92,17 @@ public class TerrainData : ScriptableObject
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+public class Layer{
+    public Texture2D texture;
+    public Color tint;
+    [Range(0,1)]
+    public float tintStrength;
+    [Range(0,1)]
+    public float startHeight;
+    [Range(0,1)]
+    public float blendStrength;
+    public float textureScale;
 }
