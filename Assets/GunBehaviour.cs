c5b341414a4655c3f3bfe599c1fa22fc0d79c547
CasperHuikeshoven <casperhuik05@gmail.com>
2024-04-29 16:31:31 +0200

    Added network bullet

--- Assets/GunBehaviour.cs
+++ Assets/GunBehaviour.cs
@@ -24,6 +24,7 @@ public class GunBehaviour : NetworkBehaviour
     public void Shoot(){
         GameObject spawnedBullet = Instantiate(guns[0].bullet, guns[0].spawnPoint.position, guns[0].spawnPoint.rotation);
         spawnedBullet.GetComponent<Rigidbody>().velocity = guns[0].spawnPoint.forward * guns[0].bulletVelocity;
+        NetworkServer.Spawn(spawnedBullet, connectionToServer);
         Destroy(spawnedBullet, 5);
     }
 }
