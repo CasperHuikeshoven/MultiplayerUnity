d02449042a553d7c071ee17baad146fa6538ba26
CasperHuikeshoven <casperhuik05@gmail.com>
2024-04-29 15:34:41 +0200

    Only authorized player can shoot gun

---
+++ Assets/GunBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GunBehaviour : NetworkBehaviour
+{
+
+    public Gun[] guns;
+    public string shootKey;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && hasAuthority){
+            Shoot();
+        }
+    }
+
+    public void Shoot(){
+        GameObject spawnedBullet = Instantiate(guns[0].bullet, guns[0].spawnPoint.position, guns[0].spawnPoint.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = guns[0].spawnPoint.forward * guns[0].bulletVelocity;
+        Destroy(spawnedBullet, 5);
+    }
+}
+
+[System.Serializable]
+public class Gun{
+    public GameObject gun;
+    public Transform spawnPoint;
+    public GameObject bullet;
+    public float bulletVelocity;
+    
+}
